df430fce2de4d081f5af7728c54d87ee16657968
Kristoffer Snabb <kristoffer@kristoffer-desktop.(none)>
2011-03-10 17:28:59 +0200

    update methods added for the model and doctests written for the update save and delete functionality

--- softgis_feature/models.py
+++ softgis_feature/models.py
@@ -25,6 +25,33 @@ class Feature(gis_models.Model):
     A Feature is defined by structure as a geojson feature
     containing a geometry object and has properties in
     the Property model.
+    
+    >>> u = User.objects.create_user('test-feature','')
+    >>> f = Feature(geometry = 'POINT(2 3)', user = u)
+    >>> f.expire_time
+    >>> f.create_time
+    >>> f.save()
+    >>> type(f.create_time)
+    <type 'datetime.datetime'>
+    >>> from django.contrib.gis.gdal import OGRGeometry
+    >>> geos = OGRGeometry(json.dumps({'type':'point', 'coordinates': [2, 3]})).geos
+    >>> f_temp = f.update(geometry = geos) #same geom should not update
+    >>> f_temp.id == f.id
+    True
+    >>> geos = OGRGeometry(json.dumps({'type':'point', 'coordinates': [4, 4]})).geos
+    >>> f_temp = f.update(geometry = geos) #different geom should update
+    >>> f_temp.id == f.id
+    False
+    >>> type(f.expire_time) #update should have expired this
+    <type 'datetime.datetime'>
+    >>> f2 = Feature.objects.get(id = f.id)
+    >>> f2.expire_time == f.expire_time
+    True
+    >>> type(f_temp.expire_time) #the current feature should not be expired
+    <type 'NoneType'>
+    >>> f_temp.delete()
+    >>> type(f_temp.expire_time)
+    <type 'datetime.datetime'>
     """
     geometry = gis_models\
                 .GeometryField(srid=settings.SPATIAL_REFERENCE_SYSTEM_ID)
@@ -38,8 +65,30 @@ class Feature(gis_models.Model):
     def delete(self, *args, **kwargs):
         self.expire_time = datetime.datetime.today()
         
+        super(Feature, self).save(*args, **kwargs)
         return None
     
+    def update(self, geometry=None, *args, **kwargs):
+        """
+        This function updates the feature if the new geometry
+        is different from the saved one.
+        
+        Also if feature is updated it returns a new feature
+        with the new geometry and sets expired time on the
+        old one.
+        """
+        if(geometry != None and geometry != self.geometry):
+            #set old feature as expired
+            self.expire_time = datetime.datetime.today()
+            super(Feature, self).save(*args, **kwargs)
+        
+            #save the new feature
+            new_feature = Feature(user = self.user, geometry = geometry)
+            new_feature.save()
+            return new_feature
+        
+        return self   
+        
     def geojson(self):
 
         properties = None
@@ -49,7 +98,9 @@ class Feature(gis_models.Model):
             # this should be changed to support different time queries
             properties = Property.objects.filter(feature__exact=self)\
                                             .latest('create_time')
+
             properties_dict = json.loads(properties.json_string)
+
         except ObjectDoesNotExist:
             properties_dict = {}
             
@@ -76,6 +127,28 @@ class Feature(gis_models.Model):
 class Property(models.Model):
     """
     This model saves all the properties for the features,
+    
+    >>> u = User.objects.create_user('test-property','')
+    >>> f = Feature(geometry = 'POINT(2 3)', user = u)
+    >>> f.save()
+    >>> p = Property(feature = f, json_string='{"some_prop": 140}')
+    >>> p.save()
+    >>> p.expire_time
+    >>> type(p.create_time)
+    <type 'datetime.datetime'>
+    >>> p1 = p.update('{"some_prop": 140}') #should not update same json
+    >>> p1.id == p.id
+    True
+    >>> p1 = p.update('{"some_prop": 141}') #should update different json
+    >>> p1.id == p.id
+    False
+    >>> type(p.expire_time)
+    <type 'datetime.datetime'>
+    >>> type(p1.expire_time)
+    <type 'NoneType'>
+    >>> p1.delete()
+    >>> type(p1.expire_time)
+    <type 'datetime.datetime'>
     """
     feature = models.ForeignKey(Feature)
     json_string = models.TextField()
@@ -86,30 +159,14 @@ class Property(models.Model):
     objects = models.Manager()
     
     if USE_MONGODB:
-        mongodb = MongoDBManager(collection_name='feature_properties') #manager for querying json
+        mongodb_collection_name = 'feature_properties'
+        mongodb = MongoDBManager(collection_name = mongodb_collection_name) #manager for querying json
     
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):    
+        #save this new property
+        super(Property, self).save(*args, **kwargs)
         
-        current_property = None
-        current_property = \
-            Property.objects.filter(feature__exact = self.feature)
-        
-        if current_property.count() == 0:
-            #save this new property
-            super(Property, self).save(*args, **kwargs)
-
-            self.save_json_to_mongodb()
-            
-        else:
-            current_property = current_property.latest('create_time')
-            if(current_property.json_string != self.json_string):
-                #delete current property
-                current_property.delete()
-    
-                #save this new property
-                super(Property, self).save(*args, **kwargs)
-                
-                self.save_json_to_mongodb()
+        self.save_json_to_mongodb()
                     
                 
     def save_json_to_mongodb(self):
@@ -129,5 +186,20 @@ class Property(models.Model):
         
         return None
     
+    def update(self, json_string, *args, **kwargs):
+        if self.json_string != json_string:
+            #set old feature as expired
+            self.expire_time = datetime.datetime.today()
+            super(Property, self).save(*args, **kwargs)
+            
+            #save the new property
+            new_property = Property(feature = self.feature,
+                                    json_string = json_string)
+            new_property.save()
+            
+            return new_property
+        
+        return self
+        
     def __unicode__(self):
         return self.json_string
