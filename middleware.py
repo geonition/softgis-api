f898a1c98d1abc51bc9739399baca72050089db8
Kristoffer Snabb <kristoffer@kristoffer-desktop.(none)>
2011-03-18 15:02:07 +0200

    the middleware modified to handle options requests correctly

--- middleware.py
+++ middleware.py
@@ -6,15 +6,27 @@ class CrossSiteAccessMiddleware(object):
     to the reponse.
     """
     
-    def process_response(self, request, response):
-        
-        if request.method == "OPTIONS":
-            response = HttpResponse()
+    def _set_access_headers(response):
         
         response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, PUT, OPTIONS'
         response['Access-Control-Max-Age'] = 1000
         response['Access-Control-Allow-Headers'] = '*'
+        
+    
+    def process_request(self, request):
+        if request.method == "OPTIONS":
+        
+            self._set_access_headers(response)
+        
+            return HttpResponse()
+            
+        return None
+    
+    
+    def process_response(self, request, response):
+        
+        self._set_access_headers(response)
         
         return response
     
