41032acfe48b13b32ec78af800fec0507f575a30
Kristoffer Snabb <kristoffer@kristoffer-desktop.(none)>
2011-03-22 14:55:30 +0200

    the mongodb tests and the hiroty tests added for the profile application

--- softgis_profile/tests.py
+++ softgis_profile/tests.py
@@ -1,23 +1,196 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth import login as django_login
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+import time
+import sys
+import settings
+import datetime
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+if sys.version_info >= (2, 6):
+    import json
+else:
+    import simplejson as json
+    
 
->>> 1 + 1 == 2
-True
-"""}
 
+class ProfileTest(TestCase):
+    """
+    This class tests the profile app REST
+    """
+    
+    def setUo(self):
+        self.client = Client()
+        
+        user = User.objects.create_user('profileuser', '', 'passwd')
+        user.save()
+    
+    
+    def test_mongodb(self):
+        USE_MONGODB = getattr(settings, "USE_MONGODB", False)
+        
+        #if mongodb is not in use do not run the tests
+        if USE_MONGODB:
+            
+            user = User.objects.create_user('profilemongo', '', 'passwd')
+            user.save() #something wrong sith setup?
+        
+            self.client.login(username='profilemongo', password='passwd')
+            
+            #save some values into the database
+            profile_dict = {
+                "age": 30,
+                "gender": "M",
+                "happy": True
+            }
+            
+            response = self.client.post(reverse('api_profile'),
+                                     json.dumps(profile_dict),
+                                     content_type='application/json')
+            
+            
+            #retrieve object out of scope age__max=29
+            response = self.client.get(reverse('api_profile') + "?age__max=29")
+          
+            response_dict = json.loads(response.content)
+            
+            self.assertEquals(len(response_dict),
+                              0,
+                              "The profile query should have returned 0 profiles")
+            
+            #retrieve object out of scope age__min=45
+            response = self.client.get(reverse('api_profile') + "?age__min=45")
+            response_dict = json.loads(response.content)
+            self.assertEquals(len(response_dict),
+                              0,
+                              "The profile query should have returned 0 profile")
+            
+            #retrieve one object age=30
+            response = self.client.get(reverse('api_profile') + "?age=30")
+            response_dict = json.loads(response.content)
+            self.assertEquals(len(response_dict),
+                              1,
+                              "The profile query should have returned 1 profile")
+            
+            #retrieve objects in scope age__min=41&age__max=45
+            response = self.client.get(reverse('api_profile') + "?age__min=29&age__max=31")
+            response_dict = json.loads(response.content)
+            self.assertEquals(len(response_dict),
+                              1,
+                              "The profile query should have returned 1 profile")
+            
+            
+            #retrieve objects with string value
+            response = self.client.get(reverse('api_profile') + "?gender=%s" % "M")
+            response_dict = json.loads(response.content)
+            self.assertEquals(len(response_dict),
+                              1,
+                              "The profile query should have returned 1 profile")
+
+            
+            #retrieve objects with boolean value
+            response = self.client.get(reverse('api_profile') + "?happy=true")
+            response_dict = json.loads(response.content)
+            self.assertEquals(len(response_dict),
+                              1,
+                              "The profile query should have returned 1 profile")
+        
+    def test_history(self):
+        
+        user = User.objects.create_user('profilehistory', '', 'passwd')
+        user.save() #something wrong sith setup?
+        
+        res = self.client.login(username='profilehistory', password='passwd')
+       
+        
+        #save some values into the database
+        profile_dict = {
+            "age": 30,
+            "gender": "M",
+            "happy": True
+        }
+        
+        profile_dict2 = {
+            "age": 35,
+            "gender": "F",
+            "happy": False
+        }
+        
+        before_create = datetime.datetime.now()
+        
+        time.sleep(1) #wait a second to get smart queries
+        
+        response = self.client.post(reverse('api_profile'),
+                                json.dumps(profile_dict),
+                                content_type='application/json')
+        
+        self.assertEquals(200,
+                          response.status_code,
+                          "Saving of profile did not return 200 OK")
+        
+        #wait a little bit to get difference
+        time.sleep(1)
+        after_create = datetime.datetime.now()
+        time.sleep(1)
+        
+        #update
+        response = self.client.post(reverse('api_profile'),
+                                json.dumps(profile_dict2),
+                                content_type='application/json')
+        
+        self.assertEquals(200,
+                          response.status_code,
+                          "Update of profile did not return 200 OK")
+        
+        #delete one and check that the next delete does not affect it
+        time.sleep(1)
+        after_update = datetime.datetime.now()
+        
+        #start querying
+        
+        response = self.client.get(reverse('api_profile') + \
+                                   "?time=%i-%i-%i-%i-%i-%i" % (before_create.year,
+                                                            before_create.month,
+                                                            before_create.day,
+                                                            before_create.hour,
+                                                            before_create.minute,
+                                                            before_create.second))
+        
+        response_dict = json.loads(response.content)
+        
+        
+        self.assertEquals(response_dict,
+                          [],
+                          "A profile query before created profile did not return empty list")
+        
+        response = self.client.get(reverse('api_profile') + \
+                                   "?time=%i-%i-%i-%i-%i-%i" % (after_create.year,
+                                                            after_create.month,
+                                                            after_create.day,
+                                                            after_create.hour,
+                                                            after_create.minute,
+                                                            after_create.second))
+        
+        response_dict = json.loads(response.content)
+        
+        
+        self.assertEquals(response_dict,
+                          [profile_dict],
+                          "The returned dict before update was not correct")
+        
+        response = self.client.get(reverse('api_profile') + \
+                                   "?time=%i-%i-%i-%i-%i-%i" % (after_update.year,
+                                                            after_update.month,
+                                                            after_update.day,
+                                                            after_update.hour,
+                                                            after_update.minute,
+                                                            after_update.second))
+        
+        response_dict = json.loads(response.content)
+        
+        self.assertEquals(response_dict,
+                          [profile_dict2],
+                          "The returned dict after update was not correct")
--- softgis_profile/views.py
+++ softgis_profile/views.py
@@ -3,13 +3,11 @@ from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 from django.http import HttpResponseForbidden
 from django.utils import translation
-
-from softgis_profile.models import ProfileValue
-from softgis_profile.models import StaticProfileValue
-from softgis_profile.models import get_profiles
+from softgis_profile.models import Profile
+from django.core.exceptions import ObjectDoesNotExist
 
 import settings
-
+import datetime
 import sys
 
 if sys.version_info >= (2, 6):
@@ -19,12 +17,47 @@ else:
 
 # set the ugettext _ shortcut
 _ = translation.ugettext
-    
+
+USE_MONGODB = getattr(settings, "USE_MONGODB", False)
+
 def profile(request):
     """
     This method handles the profile part of the
     REST api.
     """
+    def parse_time(time_string):
+        """
+        Helper function to parse a POST or GET time
+        from the following format:
+        yyyy-mm-dd-HH-MM-SS
+        
+        yyyy - year
+        mm - month
+        dd - day
+        HH - hour
+        MM - minute
+        SS - second
+        
+        The less accurate time value have to be given before any less
+        accurate time value.
+        
+        returns a datetime.datetime instance
+        """
+        time_accuracy = time_string.count('-')
+        if time_accuracy == 0:
+            return datetime.datetime.strptime(time_string, "%Y")
+        elif time_accuracy == 1:
+            return datetime.datetime.strptime(time_string, "%Y-%m")
+        elif time_accuracy == 2:
+            return datetime.datetime.strptime(time_string, "%Y-%m-%d")
+        elif time_accuracy == 3:
+            return datetime.datetime.strptime(time_string, "%Y-%m-%d-%H")
+        elif time_accuracy == 4:
+            return datetime.datetime.strptime(time_string, "%Y-%m-%d-%H-%M")
+        elif time_accuracy == 5:
+            return datetime.datetime.strptime(time_string, "%Y-%m-%d-%H-%M-%S")
+            
+            
     if not request.user.is_authenticated():
         return HttpResponseForbidden(_("The request has to be made by an signed in user"))
         
@@ -36,12 +69,69 @@ def profile(request):
         
         #filter according to permissions
         if(request.user.has_perm('can_view_profiles')):
-            profile_queryset = ProfileValue.objects.all()
+            profile_queryset = Profile.objects.all()
         else:
             profile_queryset = \
-                    ProfileValue.objects.filter(user__exact = request.user)
+                    Profile.objects.filter(user__exact = request.user)
+        
+        mongo_query = {}
+        
+        #set up the query
+        for key, value in limiting_param:
+            if key == 'user_id':
+                profile_queryset = profile_queryset.filter(user_id__exact = value)
+                
+            elif key == 'time':
+                dt = parse_time(value)
+                profile_qs_expired = profile_queryset.filter(create_time__lte = dt)
+                profile_qs_expired = profile_qs_expired.filter(expire_time__gte = dt)
+                profile_qs_not_exp = profile_queryset.filter(create_time__lte = dt)
+                profile_qs_not_exp = profile_qs_not_exp.filter(expire_time = None)
+                profile_queryset = profile_qs_not_exp | profile_qs_expired
+                
+            elif USE_MONGODB:
+                key = str(key)
+                if value.isnumeric():
+                    value = int(value)
+                elif value == "true":
+                    value = True
+                elif value == "false":
+                    value = False
+                
+                key_split = key.split('__')
+                command = ""
+                if len(key_split) > 1:
+                    command = key_split[1]
+                    key = key_split[0]
+                
+                if command == "max":
 
-        profile_list = get_profiles(limiting_param, profile_queryset)
+                    if mongo_query.has_key(key):
+                        mongo_query[key]["$lte"] = value
+                    else:
+                        mongo_query[key] = {}
+                        mongo_query[key]["$lte"] = value
+                        
+                elif command == "min":
+
+                    if mongo_query.has_key(key):
+                        mongo_query[key]["$gte"] = value
+                    else:
+                        mongo_query[key] = {}
+                        mongo_query[key]["$gte"] = value
+
+                elif command == "":
+                    mongo_query[key] = value
+        
+        #filter the queries acccording to the json
+        if len(mongo_query) > 0:
+            qs = Profile.mongodb.find(mongo_query)
+            profile_queryset = profile_queryset.filter(id__in = qs.values_list('id', flat=True))
+            
+            
+        profile_list = []
+        for prof in profile_queryset:
+            profile_list.append(prof.json())
         
         return HttpResponse(json.dumps(profile_list))
     
@@ -54,42 +144,19 @@ def profile(request):
         except ValueError, err:
             return HttpResponseBadRequest("JSON error: " + str(err.args))
 
-        allow_notifications = values.pop('allow_notifications', False)
-        birthyear = values.pop('birthyear', None)
-        gender = values.pop('gender', u'')
-        email = values.pop('email', None)
-        
-        static_profile_values = \
-                StaticProfileValue.objects.filter(user__exact = request.user)
-        
-        if len(static_profile_values) == 0:
-            static_profile_values = \
-                                StaticProfileValue(user_id = request.user.id)
-        else:
-            static_profile_values = static_profile_values[0]
-            
-        static_profile_values.allow_notifications = allow_notifications
-        static_profile_values.birthyear = birthyear
-        static_profile_values.gender = gender
-        static_profile_values.email = email
-
+        current_profile = None
         try:
-            static_profile_values.full_clean()
-        except ValidationError, err:
-            return HttpResponseBadRequest(json.dumps(err.message_dict))
-
-        static_profile_values.save()
+            current_profile = Profile.objects.latest('create_time')
+            
+            current_profile.update(json.dumps(values))
+            
+        except ObjectDoesNotExist:
+            
+            new_profile_value = Profile(user = request.user,
+                                       json_string = json.dumps(values))
+            new_profile_value.save()
+            
         
-        #confirm email TODO should use own version of confirmation
-        if not email == "" and not email == None:
-            email_addr = EmailAddress.objects.add_email(request.user, email)
-            print "here"
-            if email_addr == None:
-                print "none"
-                #django.db.connection.close()
-                
-        new_profile_value = ProfileValue(user = request.user,
-                                        json_string = json.dumps(values))
-        new_profile_value.save()
+        return HttpResponse(_("The profile has been saved"))
         
-    return HttpResponse("")
+    return HttpResponseBadRequest(_("This function only support GET and POST methods"))
